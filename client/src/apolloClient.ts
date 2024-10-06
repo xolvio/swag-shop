f5e57a97668766976b82829af9e30e5f14279cba
Sam J Combs <samueljcombs@gmail.com>
2024-10-06 15:46:49 -0400

    fix: correct default graphql address

--- client/src/apolloClient.ts
+++ client/src/apolloClient.ts
@@ -9,8 +9,8 @@ import { setContext } from "@apollo/client/link/context";
 const useMockServer = process.env.REACT_APP_USE_MOCK_SERVER === "true";
 
 const httpLink = new HttpLink({
-  uri: "http://localhost:3456/Xolvio-Swag-Shop-Demo/current/graphql",
-  // uri: "http://localhost:4000/",
+  // uri: "http://localhost:3456/Xolvio-Swag-Shop-Demo/current/graphql",
+  uri: "http://localhost:4000/",
 });
 
 // Add headers using setContext
