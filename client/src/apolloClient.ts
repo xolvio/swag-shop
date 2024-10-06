d729b6d2b09d5036d2d5ebc4fd7ace7ea828d854
Sam J Combs <samueljcombs@gmail.com>
2024-10-06 15:39:25 -0400

    feat: installed instant-mock client

--- client/src/apolloClient.ts
+++ client/src/apolloClient.ts
@@ -4,20 +4,26 @@ import {
   ApolloLink,
   HttpLink,
 } from "@apollo/client";
-import { instantMockLink } from "./instantMockLink";
+import { setContext } from "@apollo/client/link/context";
 
 const useMockServer = process.env.REACT_APP_USE_MOCK_SERVER === "true";
 
 const httpLink = new HttpLink({
-  uri: useMockServer
-    ? "http://localhost:3001/graphql"
-    : "http://localhost:4000/graphql",
+  uri: "http://localhost:3456/Xolvio-Swag-Shop-Demo/current/graphql",
+  // uri: "http://localhost:4000/",
 });
 
+// Add headers using setContext
+const authLink = setContext((_, { headers }) => ({
+  headers: {
+    "mocking-sequence-id": "full-stack-blitz",
+    ...headers,
+  },
+}));
+
 const client = new ApolloClient({
-  link: ApolloLink.from([instantMockLink, httpLink]),
+  link: ApolloLink.from([authLink, httpLink]),
   cache: new InMemoryCache(),
 });
 
 export default client;
-
